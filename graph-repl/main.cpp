48a14838d96e32760940693834d4ef9f1c36a8c0
fons haffmans <fons.haffmans@gmail.com>
2012-07-07 15:57:07 -0400

    Initial Commit

---
+++ graph-repl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  graph-repl
+//
+//  Created by Alfons Haffmans on 7/7/12.
+//  Copyright (c) 2012 mohegan skunk works. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+      // insert code here...
+      std::cout << "Hello, World!\n";
+    return 0;
+}
+
